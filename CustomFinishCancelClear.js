491b78d78001ddc4b178f210eac08ff0d100f5e6
nvmsridhar <48832621+nvmsridhar@users.noreply.github.com>
2020-11-11 18:54:16 +0530

    Update CustomFinishCancelClear.js

--- CustomFinishCancelClear.js
+++ CustomFinishCancelClear.js
@@ -10,7 +10,7 @@ define(function () {
 		var el = oControlHost.container;
 		el.innerHTML =
 		'<div><style>' +
-			'.myButtonDemo { border-radius: 20px;height: 28px;width: 65px;cursor: pointer;/* margin-left: 10px; */color: white;font-size: 14px;/* padding: 6px 12px 6px 12px; */background-color: #336699;border: 0px solid; }' + '.myButtonDemo:hover { color: grey;background-color: #132777; }' +
+			'.myButtonDemo { border-radius: 20px;height: 28px;width: 65px;cursor: pointer;color: White;font-size: 14px;background-color: #336699;border: 0px solid; }' + '.myButtonDemo:hover { color: White;background-color: #132777; }' +
 		'</style>' +
 		'<button class="myButtonDemo btnFinish" type="button">Go</button><br><br>' +
 		'<button class="myButtonDemo btnClear" type="button">Reset</button></div>';
