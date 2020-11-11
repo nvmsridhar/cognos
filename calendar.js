1ef041f1046fc8561983c5ba1d529250924094f3
nvmsridhar <48832621+nvmsridhar@users.noreply.github.com>
2020-11-11 20:33:16 +0530

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,21 +1,22 @@
-define(function () {
-	"use strict";
+define(["jquery"], function($) {
+"use strict";
 
 function CalendarSpace()
 {
 };
 
 CalendarSpace.prototype.draw = function( oControlHost ) {
-	var tableElements = document.getElementsByTagName("table");
+// 	var tableElements = document.getElementsByTagName("table");
 
-	for(let i=0; i<tableElements.length;i++){
-	    element= tableElements[i].getAttributeNode("specname");
-		if(element!=null){
-			if(element.value=="selectDate"){
-				tableElements[i].style.width="auto";
-			}
-		}
-	}
+// 	for(let i=0; i<tableElements.length;i++){
+// 	    element= tableElements[i].getAttributeNode("specname");
+// 		if(element!=null){
+// 			if(element.value=="selectDate"){
+// 				tableElements[i].style.width="auto";
+// 			}
+// 		}
+// 	}
+	$("table").find("[specname='selectDate']").css("width", "auto");
 };
 
 return CalendarSpace;
