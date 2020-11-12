abbaa264878a369fb1d06bb8653db4c0ea9fea37
nvmsridhar <48832621+nvmsridhar@users.noreply.github.com>
2020-11-12 20:07:24 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -6,8 +6,12 @@ function CalendarSpace()
 };
 
 CalendarSpace.prototype.draw = function( oControlHost ) {
+	var conf= oControlHost.configuration;
+	if(!conf){
+		conf = {}
+	}
 	$("table").find("[specname='selectDate']").css("width", "auto");
-	$(".clsCCHLDatePickerButton").css("padding-left", "0px");
+	$(".clsCCHLDatePickerButton").css("padding-left", (!conf.space ? "0px" : conf.space));
 };
 
 return CalendarSpace;
