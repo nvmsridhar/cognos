985e2abde3704e835e9a3e737f9a4761b713e3ec
nvmsridhar <48832621+nvmsridhar@users.noreply.github.com>
2020-11-12 18:43:37 +0530

    Update calendar_and_multibox.js

--- calendar_and_multibox.js
+++ calendar_and_multibox.js
@@ -12,7 +12,7 @@ CalendarSpace.prototype.draw = function( oControlHost ) {
 	}
 	$("table").find("[specname='selectDate']").css("width", "auto");
 	$(".clsCCHLDatePickerButton").css("padding-left", (!conf.space ? "0px" : conf.space));
-	$("table[specname=selectDate].clsCCHLTextWidget").css({minWidth: "", width: "80px !important", minHeight: "20px", fontSize: "8pt"});
+	$("input.clsCCHLTextWidget").css({minWidth: "", width: "80px !important", minHeight: "20px", fontSize: "8pt"});
 	$("img.clsCCHLDatePickerButton").css({width: "18px", height: "18px", cursor: "pointer"});
 	$(".clsListViewReportView").css("line-height", (!conf.lineHeight ? "15px" : conf.lineHeight));
 };
