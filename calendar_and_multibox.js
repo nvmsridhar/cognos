e7aef92e914708492b4a0cfb4a974ad451be4331
nvmsridhar <48832621+nvmsridhar@users.noreply.github.com>
2020-11-12 12:55:33 +0530

    Update and rename calendar.js to calendar_and_multibox.js

--- calendar_and_multibox.js
+++ calendar_and_multibox.js
@@ -7,6 +7,9 @@ function CalendarSpace()
 
 CalendarSpace.prototype.draw = function( oControlHost ) {
 	var conf= oControlHost.configuration;
+	if(!conf){
+		conf = {}
+	}
 	$("table").find("[specname='selectDate']").css("width", "auto");
 	$(".clsCCHLDatePickerButton").css("padding-left", "0px");
 	$(".clsListViewReportView").css("line-height", (!conf.lineHeight ? "15px" : conf.lineHeight));
