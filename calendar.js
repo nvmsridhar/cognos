52ce7f51dc60183e57134c9bcd17ad9ece9da6d4
nvmsridhar <48832621+nvmsridhar@users.noreply.github.com>
2020-11-12 12:47:57 +0530

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -6,19 +6,10 @@ function CalendarSpace()
 };
 
 CalendarSpace.prototype.draw = function( oControlHost ) {
-// 	var tableElements = document.getElementsByTagName("table");
-
-// 	for(let i=0; i<tableElements.length;i++){
-// 	    element= tableElements[i].getAttributeNode("specname");
-// 		if(element!=null){
-// 			if(element.value=="selectDate"){
-// 				tableElements[i].style.width="auto";
-// 			}
-// 		}
-// 	}
+	var conf= oControlHost.configuration;
 	$("table").find("[specname='selectDate']").css("width", "auto");
 	$(".clsCCHLDatePickerButton").css("padding-left", "0px");
-	$(".clsListViewReportView").css("line-height", "15px");
+	$(".clsListViewReportView").css("line-height", (!conf.lineHeight ? "15px" : conf.lineHeight));
 };
 
 return CalendarSpace;
